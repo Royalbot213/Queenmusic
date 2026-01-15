5b0a84e6e4f54ab838bef2f3d95c32641dfaa872
Royalbot213 <143687906+Royalbot213@users.noreply.github.com>
2026-01-15 09:11:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Royalsongmusic/Meeramusic",
+    "https://github.com/Royalbot213/Queenmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
